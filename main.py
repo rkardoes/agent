d18fda80b528eadf0ea3368c2ae3affa8ffefa6b
Ryan Kardoes <rkardoes@gmail.com>
2026-02-28 12:20:54 -0500

    added user prompting and verbose flag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,10 +12,30 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    print("Hello from agent!")
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=content)
-    print(response.text)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    user_prompt = args.user_prompt
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    
+    response = client.models.generate_content(model="gemini-2.5-flash", contents= messages)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    
+    if response.usage_metadata == None:
+        raise RuntimeError("response.usage_metadata is None, check for API error")
+    
+    if args.verbose:
+        print(f"User Prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+        print(response.text)
+        
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
