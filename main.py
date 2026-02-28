e0cd9210680eb9db8df4c278a8e9f6471606b2ef
Ryan Kardoes <rkardoes@gmail.com>
2026-02-28 11:31:02 -0500

    first commit for agent project, set up api

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+
+def main():
+    print("Hello from agent!")
+    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=content)
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
