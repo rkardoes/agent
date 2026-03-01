2494dd757e3db66c8c2a987634797f5779a8a099
Ryan Kardoes <rkardoes@gmail.com>
2026-03-01 12:56:50 -0700

    partial get file content function, needs finishing

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    
+    try:
+    
+        abs_dir_path = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(abs_dir_path, file_path))
+        valid_target_path = os.path.commonpath([abs_dir_path, target_path]) ==  abs_dir_path
+        
+        if not valid_target_path:
+            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+            
+        if not os.path.isfile(target_path):
+            f'Error: File not found or is not a regular file: "{file_path}"'
+            
+        with open(file_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        # After reading the first MAX_CHARS...
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        
+        return content
+            
+    except Exception as e:
+        return f"Error: exception \"{e}\" was raised"
